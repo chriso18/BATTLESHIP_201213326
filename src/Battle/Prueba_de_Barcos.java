074768a96485cd9e068ac1fcda1a286fa7a33593
chriso18 <christian.sierra.elias@gmail.com>
2015-03-24 12:44:56 -0600

    colocando lo necesario
    un array de tipo entero para guardar las posiciones
    y un string para guardar eso de a5 y c3

--- src/Battle/Prueba_de_Barcos.java
+++ src/Battle/Prueba_de_Barcos.java
@@ -5,6 +5,16 @@ public class Prueba_de_Barcos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		BarcoNormal barco = new BarcoNormal();
+		
+		int [] posiciones = {2,3,4,5};
+		
+		String usuarioprueba = "2";
+		
+		
+		
+		
+		
 	}
 
 }
