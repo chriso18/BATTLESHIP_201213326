15234d97694b5739753c864d9243666b5c510409
chriso18 <christian.sierra.elias@gmail.com>
2015-03-24 15:23:30 -0600

    añadiendo la clase scanner
    a demas de variables de control

--- src/Battle/Prueba_de_Barcos.java
+++ src/Battle/Prueba_de_Barcos.java
@@ -1,19 +1,29 @@
 package Battle;
 
+import java.util.Scanner;
+
 public class Prueba_de_Barcos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
 
 		BarcoNormal barco = new BarcoNormal();
+		
+		//String Usuario_invitado = "1";(1)
+		String Usuario_invitado;
+		
+		int numero_usuarios=0;
+		
+		boolean barco_esta_vivo = true;
 		
 		int [] posiciones = {2,3,4,5};
 		
 		barco.setposiciones(posiciones);
 		
-		String Usuario_invitado = "1";
 		
-		barco.Verifica_usuario(Usuario_invitado);
+		
+		//barco.Verifica_usuario(Usuario_invitado);(1)
 		
 		
 		
